54ffa5a105faa70eeb8204ac087e8eb16526bcef
samar raj <samarrajxyz@gmail.com>
2026-03-21 15:29:55 +0530

    Cleanup: Finalize one-project monorepo by removing mobile app folders

--- powerhouse_gym/android/app/src/main/kotlin/com/powerhouse/powerhouse_gym/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.powerhouse.powerhouse_gym
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- powerhouse_gym/lib/core/constants/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  // Change this to your Vercel deployed URL in production
-  static const String baseUrl = 'http://localhost:3000/api';
-  static const String jwtKey = 'jwt_token';
-  static const String userKey = 'user_data';
-}
